23b08ae695258ebf492983221fc3546441b38322
zimple kabariya <zkabari1@binghamton.edu>
2019-02-08 20:06:13 -0500

    Random Tile and Touch listeners

--- app/src/main/java/com/example/a2048/GameScreen.java
+++ app/src/main/java/com/example/a2048/GameScreen.java
@@ -2,12 +2,51 @@ package com.example.a2048;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class GameScreen extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        Button[] buttons = new Button[16];
+        static Random r = new Random();
+        int tiles[][] = new int[4][4];
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
-    }
+        addRandomNum();
+        }
+
+        // Generate random tile
+        private void addRandomNum(){
+            int i = r.nextInt((16 - 1) + 1) + 1;
+            String b_ID = "button" + i;
+            int ID = getResources().getIdentifier(b_ID, "id", getPackageName());
+            buttons[i] = ((Button) findViewById(ID));
+            if(buttons[i].getText() != null)
+                addRandomNum();
+            else
+                buttons[i].setText("2");
+                //buttons[i].setBackgroundColor(@#000);
+        }
+
+
+
+      /*switch (number) {
+            case 0: lable.setBackgroundColor(0xffccc0b2); break;
+            case 2: lable.setBackgroundColor(0xffeee4da); break;
+            case 4: lable.setBackgroundColor(0xffede0c8); break;
+            case 8: lable.setBackgroundColor(0xfff2b179); break;
+            case 16: lable.setBackgroundColor(0xfff59563); break;
+            case 32: lable.setBackgroundColor(0xfff67c5f); break;
+            case 64: lable.setBackgroundColor(0xfff65e3b); break;
+            case 128: lable.setBackgroundColor(0xffedcf72); break;
+            case 256: lable.setBackgroundColor(0xffedc750); break;
+            case 512: lable.setBackgroundColor(0xffedc850); break;
+            case 1024: lable.setBackgroundColor(0xffecc640); break;
+            default: lable.setBackgroundColor(0xffedc22d); break;
+        } */
 }
---
+++ app/src/main/java/com/example/a2048/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.a2048;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static int time_out=5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent game = new Intent(MainActivity.this, GameScreen.class);
+                startActivity(game);
+                finish();
+            }
+        },time_out);
+    }
+}
